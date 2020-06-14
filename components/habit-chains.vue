84f739c301bf93499c5c260471a48262a2408ee3
Boris Mateev <mateev.br@gmail.com>
2020-06-13 23:08:14 -0400

    feat - scaffold for links & chain components
    - compiling, not executing properly yet

---
+++ components/habit-chains.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <form>
+    <input type="text" v-model="newChain">
+    <button :on-press="addLink()">Add Link</button>
+  </form>
+
+  <div>
+    <div v-for="link in links" :key="link.name">
+      <Link name="link.name">
+    </div>
+  <div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Link from './habit-link.vue';
+
+export default Vue.extend({
+  components: {
+    Link,
+  },
+  data: {
+    newChain: '',
+    links: [] as {name: string} [],
+  },
+  methods: {
+    addLink: function() {
+      this.links.push({
+        name: this.newChain,
+      });
+    }
+  }
+})
+
+</script>
+
+
+<style>
+
+
+</style>
