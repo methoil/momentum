acb776cd4ec8a334eb13e952aa992f7ea4259cdc
Boris Mateev <mateev.br@gmail.com>
2020-06-19 23:58:56 -0400

    feat - adding multiple habits

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "./App.css";
 import Link from "./components/link";
+import habits from "./mock-data.json";
 
 function App() {
-  const links = [];
-  for (let i = 0; i < 10; i++) {
-    links.push(<Link active={!(i % 2)}></Link>);
+  const timePeriods = [];
+  for (let habit in habits) {
+    const links = [];
+    // for mock, started 10 days ago
+    for (let i = 0; i < 10; i++) {
+      links.push(
+        <div>
+          <span>{habits[habit]}</span>
+          <span>
+            <Link active={!(i % 2)}></Link>
+          </span>
+        </div>
+      );
+    }
+
+    timePeriods.push(
+      <div>
+        <span></span>
+        <span>{links}</span>
+      </div>
+    );
   }
 
   return (
     <div className="App">
       <header className="App-header">Momentum - powerfully build habits</header>
-      <div>{links}</div>
+      <div className="links-container">{timePeriods}</div>
     </div>
   );
 }
