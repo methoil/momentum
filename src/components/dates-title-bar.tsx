a17ae926664d168544e8d37a4eebd09905dc9426
methoil <mateev.br@gmail.com>
2020-07-20 23:55:40 -0400

    feat(dates): added logic to display date labels starting from today
    - need to add empty days as false in the state
      - just did labels here

--- src/components/dates-title-bar.tsx
+++ src/components/dates-title-bar.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React from "react";
 import "../app/App.css";
 
 interface IProps {
-    history: Date [];
+  history: Date[];
 }
+const MAX_HISTORY = 365;
 
-export const TitleBar: React.FC<IProps> = ({history}) => {
+export const TitleBar: React.FC<IProps> = ({ history }) => {
+  const today = new Date().toDateString();
 
-    const today = new Date().toDateString();
-    
+  // We can expect the date info to come it sorted - as I will be passing it in
+  // we could even sort it here but it's not an efficient place to do it, eso with larger data
+  const dates: string[] = [];
+  var date = new Date();
 
-    // We can expect the date info to come it sorted - as I will be passing it in
-    // we could even sort it here but it's not an efficient place to do it, eso with larger data
-    const dates = history;
-    var date = new Date();
+  // add a day
+  date.setDate(date.getDate() + 1);
 
-    // add a day
-    date.setDate(date.getDate() + 1);
-
-    // make copy of history prop?
-    while (history.length) {
-
+  let count = 0;
+  while (count < history.length || count < MAX_HISTORY) {
+    const currDateStr = date.toDateString();
+    if (history[count].toDateString() === currDateStr) {
+      count++;
     }
+    dates.push(currDateStr);
+  }
 
+  const nameElements = dates.map((date) => (
+    <div className="date-string-in-title">{date}</div>
+  ));
 
-    const nameElements = dates.map(date => (
-        <div className="date-string-in-title">{date.toDateString()}</div>
-    ));
-
-    return (
-        <div className="time-period-container dates-title-bar">
-            <div className="date-titlebar-left-label">Habits:</div>
-            {nameElements}
-        </div>
-        );
-}
+  return (
+    <div className="time-period-container dates-title-bar">
+      <div className="date-titlebar-left-label">Habits:</div>
+      {nameElements}
+    </div>
+  );
+};
